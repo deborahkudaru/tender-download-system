05b1929ce52ca81cd66f18c5d61bf8b77f0b8534
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-10-13 15:05:34 +0100

    feat: remove old tender data and add new tender JSON structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Hero from '../components/Hero.vue';
+import TenderList from '../components/TenderList.vue';
 </script>
 
 <template>
  <Hero />   
+ <TenderList />
 </template>
